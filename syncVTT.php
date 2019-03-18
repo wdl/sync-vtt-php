37ed8c2dd6cda6649d75fcdf2ba16d55f25f0f96
Elenchus <sijongyeoil@gmail.com>
2019-03-18 18:02:18 +0900

    Enable negative time

--- syncVTT.php
+++ syncVTT.php
@@ -1,16 +1,30 @@
 <?php
     class syncVTT {
         function strToTimestamp($timeStr) {
+            $negative_flag = false;
+            if(strpos($timeStr, '-') !== false) {
+                $negative_flag = true;
+                $timeStr = str_replace('-', '', $timeStr);
+            }
             $ta = array_reverse(explode(':', $timeStr));
             if(count($ta) == 3) {
                 $ta[2] = (float)$ta[2] * 60 * 60;
             }
             $ta[1] = (float)$ta[1] * 60;
             $ta[0] = (float)$ta[0];
-            return array_sum($ta);
+            $res = array_sum($ta);
+            if($negative_flag) {
+                $res = $res * -1;
+            }
+            return $res;
         }
 
         function timestampToStr($time) {
+            $negative_flag = false;
+            if($time < 0) {
+                $negative_flag = true;
+                $time = abs($time);
+            }
             $ta = [];
             $timestampStr = explode('.', (string)$time);
             $sec = $timestampStr[0];
@@ -19,7 +33,8 @@
                 $ms = $timestampStr[1];
             }
             while(true) {
-                array_push($ta, str_pad(($sec - (floor($sec / 60) * 60)), 2, "0", STR_PAD_LEFT));
+                $new_sec = $sec - (floor($sec / 60) * 60);
+                array_push($ta, str_pad($new_sec, 2, "0", STR_PAD_LEFT));
                 $sec = floor($sec / 60);
                 if(!$sec) {
                     break;
@@ -32,14 +47,18 @@
                 $ta[2] = (string)(int)$ta[2];
             }
             $ta = array_reverse($ta);
-            return implode(':', $ta) . "." . str_pad($ms, 3, "0");
+            $res = implode(':', $ta) . "." . str_pad($ms, 3, "0");
+            if($negative_flag) {
+                $res = '-' . $res;
+            }
+            return $res;
         }
 
         function sync($input, $output, $pos) {
             $fp = fopen($input, 'r');
             $fr = fread($fp, filesize($input));
             fclose($fp);
-            preg_match_all('/([0-9:\.]+) --> ([0-9:\.]+)/', $fr, $matches, PREG_SET_ORDER);
+            preg_match_all('/(-?[0-9:\.]+) --> (-?[0-9:\.]+)/', $fr, $matches, PREG_SET_ORDER);
             foreach($matches as $match) {
                 $start = $match[1];
                 $end = $match[2];
@@ -47,7 +66,6 @@
                 $newEnd = $this->timestampToStr($this->strToTimestamp($end) + $pos);
                 $fr = str_replace($start . ' --> ' . $end, $newStart . ' --> ' . $newEnd, $fr);
             }
-
             $fp = fopen($output, 'w');
             fwrite($fp, $fr);
             fclose($fp);
